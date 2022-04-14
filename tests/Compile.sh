9e65a3c710b2fbf44db26e344fde77bd47bb1090
Ziyao <tswuyin_st127@163.com>
2022-04-14 10:32:47 +0800

    Change building progress

--- tests/Compile.sh
+++ tests/Compile.sh
@@ -5,4 +5,7 @@
 # file: /tests/Compile.sh
 # Date: 2022.04.03
 
-tcc $1 -nostdlib ../lib/Scrt1.o ../lib/libc.a -o $2 -Wall -Werror -g -O0 -pedantic -Wextra -I ../include -I ../obj/include -I ../arch/x86_64/ 
+tcc $1 -nostdlib ../lib/Scrt1.o ../lib/libc.a -o $2 -Wall -Werror -g -O0 \
+	-pedantic -Wextra -I ../include -I ../obj/include		 \
+	-I ../arch/x86_64/ /usr/lib/					 \
+	/usr/local/lib/tcc/libtcc1.a
